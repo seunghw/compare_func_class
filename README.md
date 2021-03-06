47c7a971758b30214680e2759281c80e8d880368
seunghw <ysp1307@naver.com>
2021-03-06 23:52:45 +0900

    0306

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Compare func and class
 
 본 과정은 react hook에 대해 배우는 공부자료입니다.
+
+- class에서 state 구현
+- function에서 state 구현
+- 라이프사이클구현
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
+// function 방식
 
 function App() {
   return (
@@ -23,10 +25,71 @@ function FuncComp(props) {
   //  let date = new Date().toString();// 초기값
 
   let [number, setNumber] = useState(props.initNumber);
-
   let [_date, setDate] = useState(new Date().toString());
 
-  console.log(++funcId+"%cclass=>render", funcStyle);
+  // side effect 주요한 return 의 렌더링값이 아닌 부수적인 타이밍에 실행되도록 하는것
+  useEffect(
+    // 이건 componentDidMount만 실행시키고싶을 때.
+    function () {
+      console.log(
+        "%cclass=> useEffect  (componentDidMount)" + +funcId,
+        funcStyle
+      );
+      document.title = number;
+      return function () {
+        // 정리정돈할 때 useEffect 내부에서 리턴하면 됨 == CleanUp
+        console.log(
+          "%cclass=> useEffect return (componentWillUnMount)" + +funcId,
+          funcStyle
+        );
+      };
+    },
+    [] // 1회만 실행되고싶으면 이렇게 비어놓음
+  );
+
+  useEffect(
+    function () {
+      // number가 바뀔때만 호출
+      console.log(
+        "%cclass=> useEffect number (componentDidMount & componentDidUpdate)" +
+          +funcId,
+        funcStyle
+      );
+      document.title = number;
+      return function () {
+        // 정리정돈할 때 useEffect 내부에서 리턴하면 됨 == CleanUp
+        console.log(
+          "%cclass=> useEffect number return (componentDidMount & componentDidUpdate)" +
+            +funcId,
+          funcStyle
+        );
+      };
+    },
+    [number]
+  );
+
+  useEffect(
+    function () {
+      // date가 바뀔때만 호출
+      console.log(
+        "%cclass=> useEffect date (componentDidMount & componentDidUpdate)" +
+          +funcId,
+        funcStyle
+      );
+      document.title = _date;
+      return function () {
+        // 정리정돈할 때 useEffect 내부에서 리턴하면 됨 == CleanUp
+        console.log(
+          "%cclass=> useEffect date return (componentDidMount & componentDidUpdate)" +
+            +funcId,
+          funcStyle
+        );
+      };
+    },
+    [_date]
+  );
+
+  console.log("%cclass => render" + ++funcId, funcStyle);
   return (
     <div className="container">
       <h2>function style componet</h2>
@@ -49,16 +112,15 @@ function FuncComp(props) {
     </div>
   );
 }
+
+// class 방식
+
 var classStyle = "color:red";
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
     date: new Date().toString(),
   };
-
-  componentWillMount() {
-    console.log("%cclass => componentWillMount", classStyle);
-  }
 
   componentDidMount() {
     console.log("%cclass => componentDidMount", classStyle);
